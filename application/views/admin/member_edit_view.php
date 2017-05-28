36c6e87f0b3a8b6d7ba794b0ae0bd382d7d1b6f7
tanawating <tanawating@gmail.com>
2017-05-28 15:30:24 +0700

    2nd commit

--- application/views/admin/member_edit_view.php
+++ application/views/admin/member_edit_view.php
@@ -1,7 +1,16 @@
+<?php if($_SESSION['type']=='0' ):?>
+    <?php if($rs->am_type=='1' ):?>
+        <script>
+            window.history.back();
+        </script>
+        <?php exit; ?>
+    <?php endif; ?>
+<?php endif; ?>
+
 <!DOCTYPE html>
 <html lang="th">
 <head>
-        
+
     <!-- herd -->
 
         <?php require 'separate/home/head.php';?>
@@ -15,11 +24,11 @@
         <script src="<?php echo base_url();?>assets/datetimepicker/jquery.datetimepicker.js"></script>
 
     <!-- herd -->
-        
+
 </head>
-    
+
 <body>
-    
+
     <!-- herder -->
         <div class="container">
             <?php require 'separate/home/herder.php';?>
@@ -30,27 +39,27 @@
     <!-- body -->
         <div class="container" >
             <div class="row">
-                
+
                 <div class="col-md-2">
-       
+
                         <!-- main_menu-->
                             <?php require 'separate/admin/main_menu.php';?>
 
-                </div> 
-                       
+                </div>
+
                 <div class="col-md-10">
                     <div class="thumbnail" style="height: auto!important;">
-                                
+
                                     <div class="panel panel-primary">
                                         <div class="panel-heading">
                                             <span class="glyphicon glyphicon-cog"></span>
-                                            <b><FONT SIZE=3>จัดการข้อมูลผู้ดูแลระบบ</FONT></b> <span class="glyphicon glyphicon-arrow-right"></span> <b><FONT SIZE=3>แก้ไขข้อมูลผู้ดูแลระบบ</FONT></b>                           
+                                            <b><FONT SIZE=3>จัดการข้อมูลผู้ดูแลระบบ</FONT></b> <span class="glyphicon glyphicon-arrow-right"></span> <b><FONT SIZE=3>แก้ไขข้อมูลผู้ดูแลระบบ</FONT></b>
                                         </div>
                                         <div class="panel-body">
                                         <?php if (isset($_SESSION['successEdit'])) {?>
                                             <div class="alert alert-success"> <center><span class="glyphicon glyphicon-bell fa-lg"></span> <?php echo $_SESSION['successEdit'];?></center></div>
                                         <?php }?>
-                                            
+
                                             <form method="post" action="../edit" class="form-horizontal">
                                                 <input type="hidden" class="form-control" name="txtID" value="<?php echo $rs->am_id ?>" >
                                                 <div class="form-group">
@@ -74,22 +83,22 @@
                                                 <div class="form-group">
                                                     <div class="col-sm-offset-5 col-sm-4">
                                                         <a href="<?php echo base_url('admin_manage_member/index') ?>" class="btn btn-success" role="button"><i class="fa fa-reply" aria-hidden="true"></i> กลับ</a>
-                                                        <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-saved"></span> บันทึก</button>    
+                                                        <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-saved"></span> บันทึก</button>
                                                     </div>
                                                 </div>
                                             </form>
-    
+
                                         </div>
                                     </div>
 
                     </div>
                 </div>
-                     
-            </div>   
+
+            </div>
         </div>
-        
+
     <!-- footer -->
- 
+
             <?php require 'separate/home/footer.php';?>
 
     <!-- footer -->
@@ -99,9 +108,8 @@
         modules: 'security, file',
         onModulesLoaded: function () {
         $('input[name="pass_confirmation"]').displayPasswordStrength();}});
-    </script> 
+    </script>
 
 
 </body>
 </html>
-
